ff10243e439f018a3774b3fac01f9fc239d78037
Atul Kumar <atul21190@gmail.com>
2016-07-29 21:38:50 +0530

    Changes

---
+++ GitProject/test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Manish Saini  on 29/07/16.
+//  Copyright © 2016 Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitProject/test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Manish Saini  on 29/07/16.
+//  Copyright © 2016 Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
